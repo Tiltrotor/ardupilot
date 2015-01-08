7dc6bd21eae3a11c229f2a3a410314ee31ee895e
strandt <teamtiltrotor@gmail.com>
2015-01-07 20:39:50 -0500

    Added definition for frame type "Tiltrotor-Y6" Set default standard PID values to be the same as a traditional Y-6 Enabled WP YAW BEHAVIOR to always look ahead. Any other setting could result in a crash for airplane mode AUTO. (this may not be relevant in future navigation changes)

--- ArduCopter/config.h
+++ ArduCopter/config.h
@@ -153,6 +153,7 @@
   # define RATE_YAW_I                   0.015f
 #endif
 
+
 /////////////////////////////////////////////////////////////////////////////////
 // Tiltrotor_Y6 defaults
 #if FRAME_CONFIG == TILTROTOR_Y6_FRAME
@@ -177,78 +178,7 @@
   # define RATE_YAW_IMAX_MOT            1000
   # define AUTOTUNE_ENABLED             DISABLED
 #endif
-/////////////////////////////////////////////////////////////////////////////////
-// Tiltrotor_Y6 defaults
-#if FRAME_CONFIG == TILTROTOR_Y6_FRAME
-  # define WP_YAW_BEHAVIOR_DEFAULT      WP_YAW_BEHAVIOR_LOOK_AHEAD
-  # define RATE_ROLL_P                  0.1f
-  # define RATE_ROLL_D                  0.006f
-  # define RATE_PITCH_P                 0.1f
-  # define RATE_PITCH_D                 0.006f
-  # define RATE_YAW_P                   0.150f
-  # define RATE_YAW_I                   0.015f
-  # define RATE_ROLL_P_AERO				0.150f
-  # define RATE_ROLL_I_AERO             0.100f
-  # define RATE_ROLL_D_AERO				0.004f
-  # define RATE_ROLL_IMAX_AERO           1000
-  # define RATE_PITCH_P_AERO		    0.150f
-  # define RATE_PITCH_I_AERO            0.100f
-  # define RATE_PITCH_D_AERO	        0.004f
-  # define RATE_PITCH_IMAX_AERO         1000
-  # define RATE_YAW_P_MOT				0.2f
-  # define RATE_YAW_I_MOT               0.020f
-  # define RATE_YAW_D_MOT				0.000f
-  # define RATE_YAW_IMAX_MOT            1000
-  # define AUTOTUNE_ENABLED             DISABLED
-#endif
-/////////////////////////////////////////////////////////////////////////////////
-// Tiltrotor_Y6 defaults
-#if FRAME_CONFIG == TILTROTOR_Y6_FRAME
-  # define WP_YAW_BEHAVIOR_DEFAULT      WP_YAW_BEHAVIOR_LOOK_AHEAD
-  # define RATE_ROLL_P                  0.1f
-  # define RATE_ROLL_D                  0.006f
-  # define RATE_PITCH_P                 0.1f
-  # define RATE_PITCH_D                 0.006f
-  # define RATE_YAW_P                   0.150f
-  # define RATE_YAW_I                   0.015f
-  # define RATE_ROLL_P_AERO				0.150f
-  # define RATE_ROLL_I_AERO             0.100f
-  # define RATE_ROLL_D_AERO				0.004f
-  # define RATE_ROLL_IMAX_AERO           1000
-  # define RATE_PITCH_P_AERO		    0.150f
-  # define RATE_PITCH_I_AERO            0.100f
-  # define RATE_PITCH_D_AERO	        0.004f
-  # define RATE_PITCH_IMAX_AERO         1000
-  # define RATE_YAW_P_MOT				0.2f
-  # define RATE_YAW_I_MOT               0.020f
-  # define RATE_YAW_D_MOT				0.000f
-  # define RATE_YAW_IMAX_MOT            1000
-  # define AUTOTUNE_ENABLED             DISABLED
-#endif
-/////////////////////////////////////////////////////////////////////////////////
-// Tiltrotor_Y6 defaults
-#if FRAME_CONFIG == TILTROTOR_Y6_FRAME
-  # define WP_YAW_BEHAVIOR_DEFAULT      WP_YAW_BEHAVIOR_LOOK_AHEAD
-  # define RATE_ROLL_P                  0.1f
-  # define RATE_ROLL_D                  0.006f
-  # define RATE_PITCH_P                 0.1f
-  # define RATE_PITCH_D                 0.006f
-  # define RATE_YAW_P                   0.150f
-  # define RATE_YAW_I                   0.015f
-  # define RATE_ROLL_P_AERO				0.150f
-  # define RATE_ROLL_I_AERO             0.100f
-  # define RATE_ROLL_D_AERO				0.004f
-  # define RATE_ROLL_IMAX_AERO           1000
-  # define RATE_PITCH_P_AERO		    0.150f
-  # define RATE_PITCH_I_AERO            0.100f
-  # define RATE_PITCH_D_AERO	        0.004f
-  # define RATE_PITCH_IMAX_AERO         1000
-  # define RATE_YAW_P_MOT				0.2f
-  # define RATE_YAW_I_MOT               0.020f
-  # define RATE_YAW_D_MOT				0.000f
-  # define RATE_YAW_IMAX_MOT            1000
-  # define AUTOTUNE_ENABLED             DISABLED
-#endif
+
 //////////////////////////////////////////////////////////////////////////////
 // ADC Enable - used to eliminate for systems which don't have ADC.
 //
